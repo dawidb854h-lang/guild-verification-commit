9f3ad3eef270531f002f4447080732643e4373cf
dawidb854h-lang <dawid.b854h@gmail.com>
2026-03-05 20:44:55 +0100

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:44:42.344Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T19:44:43.714Z
+Commit: Fix broken links in documentation
